2591c5c24a03df39c6d0fd8d45a963f298c90e53
Bruno Castro-Karney <brunope@cs.washington.edu>
2015-12-19 18:29:07 -0800

    added a listener thread to client

--- client.c
+++ client.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <time.h>
-
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 #include "utils.h"
+
+void *listener(void *user);
+void printp(PACKET *p);
 
 int main(int argc, char **argv) {
 
@@ -27,6 +29,9 @@ int main(int argc, char **argv) {
   // argv[1] is hostname, argv[2] is portno
   self.sockfd = connect_to_server(argv[1], argv[2]);
   printf("connected to %s at port %s\n", argv[1], argv[2]);
+  pthread_create(NULL, NULL, listener, (void *) &self);
+  printf("started socket listener");
+  
 
   // start the input loop - send all input data to the server
   char input[MSG_LEN];
@@ -34,3 +39,33 @@ int main(int argc, char **argv) {
     send_msg(&self, input);
   }
 }
+
+/**
+ * Takes a USER (cast as a void* because this is called from pthread_create),
+ * and reads data from its socket connection. Stops if the recv fails.
+ */
+void *listener(void *user) {
+  PACKET p;
+  int recvd;
+
+  // read loop
+  while (1) {
+    memset(&p, 0, sizeof(PACKET));
+    recvd = recv(((USER *)user)->sockfd, (void *)&p, sizeof(PACKET), 0);
+    if (!recvd) {
+      fprintf(stderr, "lost listener connection");
+      break;
+    } else if (recvd > 0) {
+      printp(&p);
+    }
+  }
+  pthread_exit(NULL);
+}
+
+/**
+ * Prints the contents of a PACKET
+ */
+void printp(PACKET *p) {
+  printf("%s: %s\n", p->sender_nick, p->message);
+}
+  
