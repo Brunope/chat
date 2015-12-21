5e66a8fb65c20373ac2ecf507b4306eb05e17f3e
Bruno Castro-Karney <brunope@cs.washington.edu>
2015-12-20 16:17:51 -0800

    added threads to server, comments everywhere

--- client.c
+++ client.c
@@ -30,12 +30,12 @@ int main(int argc, char **argv) {
   strcpy(self.nick, "Anonymous"); // default nick
   // argv[1] is hostname, argv[2] is portno
   self.sockfd = connect_to_server(argv[1], argv[2]);
-  printf("--- connected to %s at port %s ---\n", argv[1], argv[2]);
+  if (DEBUG == ON)
+    printf("--- connected to %s at port %s ---\n", argv[1], argv[2]);
   // start listening to the server for messages
   pthread_t threads[NUM_THREADS];
   pthread_create(&threads[0], NULL, listener, (void *) &self);
-  printf("--- started socket listener ---\n");
-  
+  if (DEBUG == ON) { printf("--- started socket listener ---\n"); }
 
   // start the input loop - send all input data to the server
   char input[MSG_LEN];
@@ -46,25 +46,23 @@ int main(int argc, char **argv) {
 
 /**
  * Takes a USER (cast as a void* because this is called from pthread_create),
- * and reads data from its socket connection. Stops if the recv fails.
+ * and prints data read from its socket connection. Stops if the recv fails.
  */
 void *listener(void *user) {
+  USER *u = user;
+  
+  // read data from the server for new messages
   PACKET p;
   int recvd;
-
-  USER *u = user;
-  // read loop
   while (1) {
     memset(&p, 0, sizeof(PACKET));
     recvd = recv(u->sockfd, (void *)&p, sizeof(PACKET), 0);
     if (!recvd) {
       fprintf(stderr, "--- lost listener connection ---\n");
-      break;
+      pthread_exit(NULL);
     } else if (recvd > 0) {
-      if (DEBUG == ON) {
-        printf("--- received %d bytes ---\n", recvd);
-      }
-      printp(&p);
+      if (DEBUG == ON) { printf("--- received %d bytes ---\n", recvd); }
+      printp(&p); // display the contents of the packet
     }
   }
   pthread_exit(NULL);
--- server.c
+++ server.c
@@ -12,8 +12,36 @@
 
 #include "utils.h"
 
-#define BACKLOG 10
+#define BACKLOG 5
+#define MAX_CLIENTS 10
 
+
+
+void *handle_client(void *sockfd);
+void send_to_all(PACKET *p);
+void add_client(int sockfd);
+void rm_client(int sockfd);
+
+// clients is a list of socket file descriptors. The indices in the range
+// [0, num_clients-1] contain the file descriptors of every client connecter
+// to the server.
+int clients[MAX_CLIENTS];
+
+// num_clients is the number of clients currently connected to the server.
+int num_clients = 0;
+
+// mutex is a pthread_mutex for protecting our client list across threads
+pthread_mutex_t mutex;
+
+// threads contains a thread for every client connected to the server. The id
+// of a thread is akin to the index of that thread's fd in the client list.
+pthread_t threads[MAX_CLIENTS];
+
+/**
+ * Supply 2 extra args.
+ * argv[1] - the address of the server to connect to.
+ * argv[2] - the port number on which to connect.
+ */
 int main(int argc, char **argv) {
 
   if (argc != 2) {
@@ -21,10 +49,10 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  int sockfd, new_fd;
+  pthread_mutex_init(&mutex, NULL);  
 
   struct addrinfo hints, *servinfo;
-  struct sockaddr_storage client_addr;
+
   socklen_t sin_size;
   char ip4[INET_ADDRSTRLEN];
 
@@ -37,36 +65,40 @@ int main(int argc, char **argv) {
     fprintf(stderr, "--- getaddrinfo error ---\n");
     return EXIT_FAILURE;
   }
-
+  int sockfd; // our listening socket
   sockfd = socket(servinfo->ai_family, servinfo->ai_socktype,
                   servinfo->ai_protocol);
   bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
   freeaddrinfo(servinfo);
 
+  // wait for connections
   listen(sockfd, BACKLOG);
-
-  printf("--- waiting for connections ---\n");
-
-  // accept() loop
+  if (DEBUG == ON) { printf("--- waiting for connections ---\n"); }
+  struct sockaddr_storage client_addr;
   while (1) {
     sin_size = sizeof(client_addr);
-    new_fd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-    if (new_fd == -1) {
+    int newfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+    if (newfd == -1) {
       perror("accept");
       continue;
     }
 
-    inet_ntop(client_addr.ss_family,
-              &(((struct sockaddr_in *)&client_addr)->sin_addr),
-              //get_in_addr((struct sockaddr *)&client_addr),
-              ip4,
-              sizeof(ip4));
-    printf("--- got connection from %s ---\n", ip4);
-    // recv()
+    // print the ipv4 address of the client
+    if (DEBUG == ON) {
+      inet_ntop(client_addr.ss_family,
+                &(((struct sockaddr_in *)&client_addr)->sin_addr),
+                ip4,
+                sizeof(ip4));
+      printf("--- got connection from %s ---\n", ip4);
+    }
+    
+    // spawn a client handler, passing the new socket fd
+    pthread_create(&threads[num_clients], NULL, handle_client, (void *)&newfd);
+    
     while (1) {
       PACKET p;
       memset(&p, 0, sizeof(PACKET));
-      int bytes = recv(new_fd, (void *)&p, sizeof(PACKET), 0);
+      int bytes = recv(newfd, (void *)&p, sizeof(PACKET), 0);
       if (!bytes) {
         fprintf(stderr, "--- connection lost ---");
         break;
@@ -77,8 +109,63 @@ int main(int argc, char **argv) {
       // print the packet
       printp(&p);
       // send the message to all connected clients
-      send_packet(new_fd, &p);
+      send_packet(newfd, &p);
     }
   }
 }
-              
+
+void *handle_client(void *sock) {
+  int sockfd = *(int *)(sock);
+  add_client(sockfd); // add the client to list of connected clients
+
+  // recv messages from the client
+  while (1) {
+    PACKET p;
+    memset(&p, 0, sizeof(PACKET));
+    int bytes = recv(sockfd, (void *)&p, sizeof(PACKET), 0);
+    if (!bytes) {
+      if (DEBUG == ON) { printf("--- connection lost from %d ---\n", sockfd); }
+      rm_client(sockfd);
+      pthread_exit(NULL);
+    }
+    if (DEBUG == ON) { printf("--- received %d bytes ---\n", bytes); }
+
+    printp(&p); // print the packet
+    send_packet(sockfd, &p); // send the message to all connected clients
+    
+  }
+  pthread_exit(NULL);
+}
+
+void send_to_all(PACKET *p) {
+  pthread_mutex_lock(&mutex);
+  // loop through all connected clients, and send p to each
+  for (int i = 0; i < num_clients; i++) {
+    send_packet(clients[i], p);
+  }
+  pthread_mutex_unlock(&mutex);
+}
+
+
+void add_client(int sockfd) {
+  pthread_mutex_lock(&mutex);
+  clients[num_clients] = sockfd;
+  num_clients++;
+  pthread_mutex_unlock(&mutex);
+}
+
+void rm_client(int sockfd) {
+  close(sockfd);
+  pthread_mutex_lock(&mutex);
+  for (int i = 0; i < num_clients; i++) {
+    if (sockfd == clients[i]) {
+      // delete the current client and move the rest down by one
+      for (int j = i; j < num_clients - 1; j++) {
+        clients[j] = clients[j + 1];
+      }
+      num_clients--;
+      break;
+    }
+  }
+  pthread_mutex_unlock(&mutex);
+}
