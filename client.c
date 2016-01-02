50004f2093f33a51ab70877ed5251bae08536c18
Bruno Castro-Karney <brunope@cs.washington.edu>
2016-01-02 11:43:04 -0800

    removed input handling from client; now it just sends input directly to server for processing. client.c now knows nothing about USERs or CLIENTs

--- client.c
+++ client.c
@@ -12,19 +12,17 @@
 #include <pthread.h>
 #include <curses.h>
 
-// #include "utils.h" mlist uses utils so we don't need to include it here
 #include "mlist.h"
 
 #define NUM_THREADS 2
 #define DEFAULT_NICK "Anon"
 #define LOG_FILE "log.txt"
   
-void *listener(void *user);
+void *listener(void *sockfd);
 void display_messages(WINDOW *display);
-void handle_input(USER *user, char *input);
 
 FILE *flog; // debug file
-int new_messages = FALSE;
+int new_messages = FALSE; // unread messages flag
 
 /**
  * Supply 2 extra args.
@@ -47,16 +45,21 @@ int main(int argc, char **argv) {
   }
 
   // initialize socket connection
-  USER self;
-  memset(&self, 0, sizeof(USER));
-  strcpy(self.nick, DEFAULT_NICK);
-  self.sockfd = connect_to_server(argv[1], argv[2]);
-  if (DEBUG == ON)
+  int sockfd = connect_to_server(argv[1], argv[2]);
+  if (sockfd < 0) {
+    if (DEBUG == ON) { fclose(flog); }
+    exit(EXIT_FAILURE);
+  } else if (DEBUG == ON) {
     fprintf(flog, "connected to %s at port %s\n", argv[1], argv[2]);
+  }
+
+  // handshake first thing after we connect
+  //handshake(sockfd); we don't really need to though since we're not getting
+  // a shared key or anything
   
   // start listening to the server for messages in the background
   pthread_t threads[NUM_THREADS];
-  pthread_create(&threads[0], NULL, listener, (void *) &self);
+  pthread_create(&threads[0], NULL, listener, (void *) &sockfd);
   if (DEBUG == ON) { fprintf(flog, "started socket listener\n"); }
 
   // initialize ncurses
@@ -115,9 +118,11 @@ int main(int argc, char **argv) {
       current_input++;
     }
 
-    // handle the message if we've got a newline and the message isn't empty
+    // send the message if we've got a newline and the message isn't empty
     if (c == 10 && current_input > input_message) {
-      handle_input(&self, input_message);
+      send(sockfd, input_message, BUFFER_LEN, 0);
+      //send_data(sockfd, input_message);
+      //handle_input(&self, input_message);
       //clean up
       current_input = input_message;
       wmove(input_win, 1, 0);
@@ -129,16 +134,57 @@ int main(int argc, char **argv) {
   }
 
   // clean up all our resources
-  mlist_free();
-  close(self.sockfd);
+  close(sockfd);
+  if (DEBUG == ON) { fclose(flog); }
 }
 
 /**
- * Takes a USER (cast as a void* because this is called from pthread_create),
- * and prints data read from its socket connection. Stops if the recv fails.
+ * Takes an IPv4 address or hostname and port number and connects. Returns
+ *  the new socket file descriptor. Prints an error message to stderr and 
+ * exits if something goes wrong.
  */
-void *listener(void *user) {
-  USER *u = user;
+int connect_to_server(const char *address, const char *port) {
+  // load the address structs with getaddrinfo()
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET; // IPv4 only
+  hints.ai_socktype = SOCK_STREAM; // stream socket
+
+  int err;
+  struct addrinfo *serv;
+  if (getaddrinfo(address, port, &hints, &serv) == -1) {
+    err = errno;
+    fprintf(stderr, "Could not look up address");
+    return err;
+  }
+    
+
+  // open a socket
+  int sockfd = socket(serv->ai_family, serv->ai_socktype, serv->ai_protocol);
+  if (sockfd == -1) {
+    err = errno;
+    fprintf(stderr, "Could not open socket\n");
+    return err;
+  }
+
+  // connect
+  if (connect(sockfd, serv->ai_addr, serv->ai_addrlen) == -1) {
+    err = errno;
+    fprintf(stderr, "Could not connect to %s at %s\n", address, port);
+    return err;
+  }
+
+  freeaddrinfo(serv); // can't forget to free
+  return sockfd;
+}
+
+/**
+ * Takes a connect()ed sockfd (cast as a void* because this is called from 
+ * pthread_create), and reads data from the connection. Each message received
+ * is added to a list of messages, (see mlist.c)
+ */
+void *listener(void *sockfd_void) {
+  int sockfd = *(int *)sockfd_void;
 
   mlist_init(); // initialize list of received messages
 
@@ -146,7 +192,7 @@ void *listener(void *user) {
   int recvd;
   while (1) { // read data from the server for new messages
     memset(&message, 0, sizeof(message));
-    recvd = recv(u->sockfd, message, sizeof(message), 0);
+    recvd = recv(sockfd, message, sizeof(message), 0);
     if (!recvd) {
       if (DEBUG == ON) { fprintf(stderr, "lost listener connection\n"); }
       pthread_exit(NULL);
@@ -157,33 +203,12 @@ void *listener(void *user) {
       }
       // add the message to the list and log it
       if (DEBUG == ON) { mlist_add_log(message, flog); }
-      else { mlist_add(message); }
-      
+      else { mlist_add(message); }     
       new_messages = TRUE; // flag the new messages
     }
   }
+  mlist_free();
   pthread_exit(NULL);
-}
-
-void handle_input(USER *user, char *input) {
-  // check for /nick
-  char *nick_cmd = "/nick";
-  if (strncmp(input, nick_cmd, strlen(nick_cmd)) == 0) {
-    // save the old nick real quick
-    char old_nick[NICK_LEN];
-    strcpy(old_nick, user->nick);
-    // set the new one to the token following the command
-    char *new_nick = input + strlen(nick_cmd) + 1;
-    strcpy(user->nick, new_nick);
-    // write an event and send it
-    char event[BUFFER_LEN];
-    sprintf(event, "--- %s is now known as %s ---", old_nick, user->nick);
-    send_data(user->sockfd, event);
-  }
-  // send the message otherwise
-  else {
-    send_msg(user, input);
-  }
 }
 
 void display_messages(WINDOW *display) {
