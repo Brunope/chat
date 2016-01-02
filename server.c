7e4a5d09eeb1002b77056891995ac0d9171ce5f8
Bruno Castro-Karney <brunope@cs.washington.edu>
2016-01-02 11:44:40 -0800

    server.c now stores the nick of connected clients instead of the client storing it

--- server.c
+++ server.c
@@ -14,18 +14,24 @@
 
 #define BACKLOG 5
 #define MAX_CLIENTS 10
+#define DEFAULT_NICK "Anon"
 
-
+typedef struct CLIENT {
+  int sockfd;
+  char nick[NICK_LEN];
+} CLIENT;
 
 void *handle_client(void *sockfd);
 void send_to_all(char *data);
-void add_client(int sockfd);
+void handle_msg(CLIENT *sender, char *msg);
+void handshake(CLIENT *client, int sockfd);
+void add_client(CLIENT *client);
 void rm_client(int sockfd);
 
 // clients is a list of socket file descriptors. The indices in the range
 // [0, num_clients-1] contain the file descriptors of every client connecter
 // to the server.
-int clients[MAX_CLIENTS];
+CLIENT clients[MAX_CLIENTS];
 
 // num_clients is the number of clients currently connected to the server.
 int num_clients = 0;
@@ -34,7 +40,7 @@ int num_clients = 0;
 pthread_mutex_t mutex;
 
 // threads contains a thread for every client connected to the server. The id
-// of a thread is akin to the index of that thread's fd in the client list.
+// of a thread is akin to the index of that thread's CLIENT in the list.
 pthread_t threads[MAX_CLIENTS];
 
 /**
@@ -89,19 +95,30 @@ int main(int argc, char **argv) {
                 sizeof(ip4));
       printf("--- got connection from %s ---\n", ip4);
     }
+
+    // handshake first thing after connecting, and store the result in client
+    CLIENT client;
+    handshake(&client, newfd);
     
-    // spawn a client handler, passing the new socket fd
-    pthread_create(&threads[num_clients], NULL, handle_client, (void *)&newfd);
+    // spawn a client handler, passing the new CLIENT
+    pthread_create(&threads[num_clients], NULL, handle_client, (void *)&client);
   }
 }
 
-void *handle_client(void *sock) {
-  int sockfd = *(int *)(sock);
-  add_client(sockfd); // add the client to list of connected clients
+// initialize client with a sockfd and nick, tell client they've connected
+void handshake(CLIENT *client, int sockfd) {
+  client->sockfd = sockfd;
+  strcpy(client->nick, DEFAULT_NICK);
+}
+
+void *handle_client(void *client_void) {
+  CLIENT *client = client_void;
+  int sockfd = client->sockfd;
+  add_client(client); // add the client to list of connected clients
 
   // recv messages from the client
   while (1) {
-    char data[MSG_LEN];
+    char data[BUFFER_LEN];
     memset(data, 0, sizeof(data));
     int bytes = recv(sockfd, (void *)data, sizeof(data), 0);
     if (!bytes) {
@@ -110,28 +127,47 @@ void *handle_client(void *sock) {
       pthread_exit(NULL);
     }
     if (DEBUG == ON) { printf("--- received %d bytes ---\n", bytes); }
-
-    printf("%s\n", data);
-    send_to_all(data); // send the message to all connected clients
+    printf("%s: %s\n", client->nick, data);
     
+    // process whatever the client sent us
+    handle_msg(client, data);
   }
   pthread_exit(NULL);
 }
 
-void send_to_all(char *data) {
+void handle_msg(CLIENT *sender, char *msg) {
+  char result[MSG_LEN]; // we'll send this out after processing input
+  // update client nick if they want to change it
+  if (strncmp(msg, "/nick ", 6) == 0) {
+    // save the old nick
+    char old_nick[NICK_LEN];
+    strcpy(old_nick, sender->nick);
+    // update with the new one
+    strcpy(sender->nick, msg + 6);
+    // send an update of the change to all connected clients
+    sprintf(result, "%s is now known as %s", old_nick, sender->nick);
+    send_to_all(result);
+  } else { // otherwise just send the message to all connected clients
+    sprintf(result, "%s: %s", sender->nick, msg);
+    send_to_all(result);
+  }
+}
+
+void send_to_all(char *msg) {
   if (DEBUG == ON) { printf("--- dispatching to %d clients ---\n", num_clients); }
   pthread_mutex_lock(&mutex);
-  // loop through all connected clients, and send p to each
+  // loop through all connected clients, and send data
   for (int i = 0; i < num_clients; i++) {
-    send_data(clients[i], data);
+    int bytes = send_data(clients[i].sockfd, msg);
+    printf("--- sent %d bytes ---\n", bytes);
   }
   pthread_mutex_unlock(&mutex);
 }
 
 
-void add_client(int sockfd) {
+void add_client(CLIENT *client) {
   pthread_mutex_lock(&mutex);
-  clients[num_clients] = sockfd;
+  clients[num_clients] = *client;
   num_clients++;
   pthread_mutex_unlock(&mutex);
 }
@@ -140,7 +176,7 @@ void rm_client(int sockfd) {
   close(sockfd);
   pthread_mutex_lock(&mutex);
   for (int i = 0; i < num_clients; i++) {
-    if (sockfd == clients[i]) {
+    if (sockfd == clients[i].sockfd) {
       // delete the current client and move the rest down by one
       for (int j = i; j < num_clients - 1; j++) {
         clients[j] = clients[j + 1];
