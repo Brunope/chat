fbfe5db5f7a499bc24aea257edd6fb17f87daedc
Bruno Castro-Karney <brunope@cs.washington.edu>
2016-01-02 11:43:40 -0800

    moved connect_to_server() to client.c

--- utils.c
+++ utils.c
@@ -13,27 +13,6 @@
 
 #include "utils.h"
 
-/**
- * Takes an IPv4 address and port number and connects. Returns the new socket
- * file descriptor. Prints an error message to stderr and exits if something
- * goes wrong.
- */
-int connect_to_server(const char *address, const char *port) {
-  // load the address structs with getaddrinfo()
-  struct addrinfo hints;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_INET; // IPv4 only
-  hints.ai_socktype = SOCK_STREAM; // stream socket
-  struct addrinfo *serv;
-  getaddrinfo(address, port, &hints, &serv);
-
-  // make a socket and connect
-  int sockfd = socket(serv->ai_family, serv->ai_socktype, serv->ai_protocol);
-  connect(sockfd, serv->ai_addr, serv->ai_addrlen);
-
-  freeaddrinfo(serv); // can't forget to free
-  return sockfd;
-}
 
 /**
  * Takes a message and USER, packages it up with the user nick and sends it.
@@ -58,9 +37,6 @@ int send_data(int sockfd, char *data) {
     fprintf(stderr, "only sent %d bytes\n", bytes);
   }
   return bytes;
-  /* if (DEBUG == ON) { */
-  /*   printf("--- sent %d bytes ---\n", bytes); */
-  /* } */
 }
 
 /**
