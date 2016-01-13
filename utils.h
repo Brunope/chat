6f0514cd07820af8c08abf06610cdb7759e79be9
Bruno Castro-Karney <brunope@cs.washington.edu>
2016-01-13 00:23:00 -0800

    nick length back to 20, total msg length back to 256

--- utils.h
+++ utils.h
@@ -3,21 +3,12 @@
 
 #define BUFFER_LEN 234
 #define NICK_LEN 20
-#define MSG_LEN BUFFER_LEN + NICK_LEN + 2 // length of serialized messages
+#define MSG_LEN BUFFER_LEN + NICK_LEN + 2  // message formatting buffer
 
-#define DEBUG 1
 #define OFF 0
 #define ON 1
+#define DEBUG ON
 
-typedef struct USER {
-  int sockfd; // user's socket descriptor
-  char nick[NICK_LEN]; // user's name
-} USER;
-
-int connect_to_server(const char *address, const char *port);
-int send_msg(USER *sender, char *msg);
-int send_data(int sockfd, char *data);
-void serialize(char *dst, char *nick, char *msg_str);
 int mod(int a, int b);
 
 #endif
