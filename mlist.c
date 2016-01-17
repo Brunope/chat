fce0286d9b1629c27cbc261802f1377b258fdafd
Bruno Castro-Karney <brunope@cs.washington.edu>
2016-01-16 21:21:35 -0800

    moved function spec from .c to .h

--- mlist.c
+++ mlist.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "mlist.h"
+#include "utils.h"
 
 /**
  * mlist uses a circular array of static size. It stores pointers to the "front"
@@ -12,37 +14,32 @@
 int mod(int a, int b);
 
 char **mlist = NULL;  // the actual front of the array
-int findex, bindex;  // indices of the abstracted front and back of the list
-int size;
+size_t findex, bindex;   // indices of the abstracted front and back of the list
+size_t size;             // number of elements
 
-/**
- * Allocate space on the heap for all the nodes that fit.
- */
-void mlist_init() {
+int mlist_init() {
   // allocate space for the pointers
   mlist = (char **) malloc(MLIST_CAPACITY * sizeof(mlist));
   if (mlist == NULL) {
     fprintf(stderr, "couldn't allocate memory for mlist\n");
-    exit(EXIT_FAILURE);
+    return -1;
   }
   // allocate space for the messages themselves
   for (int i = 0; i < MLIST_CAPACITY; i++) {
     mlist[i] = (char *) malloc(MSG_LEN);
     if (mlist[i] == NULL) {
       fprintf(stderr, "couldn't allocate memory for mlist\n");
+      return -1;
     }
   }
   findex = MLIST_CAPACITY - 1;
   bindex = findex;
   size = 0;
+  return 1;
 }
 
-/**
- * Return the message stored at the abstracted "index." For example,
- * mlist_get(0) returns the "first" and most recently added message in the list.
- */
-char *mlist_get(int index) {
-  if (index < 0 || index >= size) {
+char *mlist_get(size_t index) {
+  if (index >= size) {
     fprintf(stderr, "mlist_get index out of range\n");
     return NULL;
   }
@@ -51,11 +48,6 @@ char *mlist_get(int index) {
   return mlist[mod(findex + index, MLIST_CAPACITY)];
 }
 
-/**
- * Prepend a message to the list. If mlist is full (ie size == capacity),
- * delete the least recently added message and use its memory for the new
- * message.
- */
 void mlist_add(char *data) {
   if (mlist == NULL) {
     fprintf(stderr, "must call mlist_init() before mlist_add()\n");
@@ -63,7 +55,7 @@ void mlist_add(char *data) {
   }
   if (size == MLIST_CAPACITY) {
     // clobber the least recently added value
-    strcpy(mlist[bindex], data);
+    strncpy(mlist[bindex], data, MSG_LEN);
     // move both indices back 1, cycling around if necessary
     findex = mod(findex - 1, MLIST_CAPACITY);
     bindex = mod(bindex - 1, MLIST_CAPACITY);
@@ -74,33 +66,23 @@ void mlist_add(char *data) {
   }
 }
 
-/**
- * Return the number of messages in the mlist.
- */
-int mlist_size() {
+size_t mlist_size() {
   return size;
 }
 
-/**
- * Add a message and write the entire contents of the mlist to a file.
- */
 void mlist_add_log(char *data, FILE *flog) {
   fprintf(flog, "adding to mlist\n");
   mlist_add(data);
   fprintf(flog, "mlist contents:\n");
-  for (int i = 0; i < size; i++) {
+  for (size_t i = 0; i < size; i++) {
     fprintf(flog, "\t%s\n", mlist_get(i));
   }
 }
 
-/**
- * Free the entire mlist and reset its size to 0.
- */
 void mlist_free() {
   for (int i = 0; i < MLIST_CAPACITY; i++) {
     free(mlist[i]);
   }
   free(mlist);
   mlist = NULL;
-  size = 0;
 }
